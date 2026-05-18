c1d16da3b0e6ce16cd1a8344062baf00f15c7808
Mwahidcodes <mariawahid999@gmail.com>
2026-05-18 17:42:13 +0500

    Added sequenced Terraform scripts for infrastructure automation

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+# Security Group create karne ke liye jo ports open karega
+resource "aws_security_group" "share4good_sg" {
+  name        = "share4good-devops-sg"
+  description = "Security Group for Share4Good DevOps Pipeline"
+
+  # SSH Port 22
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Jenkins Port 8080
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # SonarQube Port 9000
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound rules (Internet access for server)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
